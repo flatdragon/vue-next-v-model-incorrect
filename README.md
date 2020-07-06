676c9ca9ca4b40eba297e2067c1c6f6fa8902551
Kochetygov Andrew <flatdragon@yandex.ru>
2020-07-06 13:49:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vue 3 incorrect v-model behaviour with UNDEFINED values
+## See behaviour on [GitHub Pages](https://flatdragon.github.io/vue-next-v-model-incorrect/).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Vue 3 v-model behaviour</h1>
+    <a href="https://github.com/flatdragon/vue-next-v-model-incorrect">See GitHub repo for source code</a>
+    <h2>Using REF</h2>
+    <div>Input value: {{ value }}</div>
+    <input type="text" v-model="value">
+    <h2>Using REACTIVE</h2>
+    <div>Input value: {{ state.value }}</div>
+    <input type="text" v-model="state.value">
+  </div>
+</template>
+
+<script>
+  import { ref, reactive } from 'vue'
+
+  export default {
+    name: 'App',
+    setup () {
+      const value = ref(undefined)
+      const state = reactive({})
+
+      return {
+        value,
+        state,
+      }
+    },
+  }
+</script>
