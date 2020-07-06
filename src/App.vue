a58f3eb1454cd70e26d1211713f46346d7d1f021
Kochetygov Andrew <flatdragon@yandex.ru>
2020-07-06 14:17:42 +0300

    Fix some markup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <h1>Vue 3 v-model behaviour</h1>
-    <a href="https://github.com/flatdragon/vue-next-v-model-incorrect/blob/master/src/App.vue">See GitHub repo for source code</a>
-    <h2>Using REF</h2>
-    <div>Input value: {{ value }}</div>
-    <input type="text" v-model="value">
-    <h2>Using REACTIVE</h2>
-    <div>Input value: {{ state.value }}</div>
-    <input type="text" v-model="state.value">
+    <div>
+      <h1>Vue 3 v-model behaviour</h1>
+      <a href="https://github.com/flatdragon/vue-next-v-model-incorrect/blob/master/src/App.vue">See GitHub repo for source code</a>
+    </div>
+
+    <div>
+      <h2>Using REF</h2>
+      <div>Input value: {{ value }}</div>
+      <input type="text" v-model="value">
+    </div>
+
+    <div>
+      <h2>Using REACTIVE</h2>
+      <div>Input value: {{ state.value }}</div>
+      <input type="text" v-model="state.value">
+    </div>
   </div>
 </template>
 
